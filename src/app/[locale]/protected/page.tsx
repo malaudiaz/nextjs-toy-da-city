c54964fd31d7779feb8d6c2a12c0e8bd3ae46278
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-05-20 16:32:21 -0400

    Eliminando importaciones que no se estaban usando

--- src/app/[locale]/protected/page.tsx
+++ src/app/[locale]/protected/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import { useAuth } from '@clerk/nextjs';
+import { useAuth } from "@clerk/nextjs";
 
 export default function ProtectedPage() {
   const { isLoaded, isSignedIn, user } = useUser();
@@ -15,17 +15,18 @@ export default function ProtectedPage() {
     return <div>Not signed in</div>;
   }
 
- const handleGetToken = async () => {
+  const userRoles = user?.publicMetadata?.roles || [];
+
+  const handleGetToken = async () => {
     try {
       // Obtiene el token JWT desde Clerk
-      const jwt = await getToken({ template: 'Toydacity' });
-      console.log('Token obtenido:', jwt);
-
+      const jwt = await getToken({ template: "Toydacity" });
+      console.log("Token obtenido:", jwt);
     } catch (error) {
-      console.error('Error al obtener token:', error);
+      console.error("Error al obtener token:", error);
     }
-  };  
-  
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -34,6 +35,14 @@ export default function ProtectedPage() {
         <div>
           <h1>Hello, {user.firstName}!</h1>
           <p>Email: {user.primaryEmailAddress?.emailAddress}</p>
+
+          <div>
+            <h2>Roles del usuario (cliente):</h2>
+            <ul>
+              {Array.isArray(userRoles) &&
+                userRoles.map((role, index) => <li key={index}>{role}</li>)}
+            </ul>
+          </div>
 
           <button onClick={handleGetToken}>
             Obtener Token y Hacer Petición
--- src/components/shared/header/Navbar.tsx
+++ src/components/shared/header/Navbar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import TopNavbar from "./TopNavbar";
 import MiddleNavbar from "./MiddleNavbar";
-import BottomNavbar from "./BottomNavbar";
 
 const Navbar = () => {
   return (
     <>
-    <TopNavbar />
-    <MiddleNavbar />
-    {/* <BottomNavbar /> */}
+      <TopNavbar />
+      <MiddleNavbar />
     </>
   );
 };
