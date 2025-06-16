069f394b8a5480e505f1466918194c110520e355
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-06-16 15:13:24 -0400

    cambio en el paginado de toy

--- src/app/[locale]/api/toys/route.ts
+++ src/app/[locale]/api/toys/route.ts
@@ -52,8 +52,8 @@ export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url!)
 
     const pagination = PaginationSchema.parse({
-      page: searchParams.get('page') || 1,
-      limit: searchParams.get('limit') || 10
+      page: parseInt(searchParams.get('page') || "1"),
+      limit: parseInt(searchParams.get('limit') || "10")
     });
 
     const filters = ToyFilterSchema.parse({
