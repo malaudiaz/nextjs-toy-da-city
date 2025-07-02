75bfaad5bdc79d5b4b56af002306a07546a2b2ad
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-07-02 14:09:44 -0400

    refactorizado

--- src/app/[locale]/post/page.tsx
+++ src/app/[locale]/post/page.tsx
@@ -3,18 +3,17 @@ import Sigin from '@/components/shared/Sigin';
 import { getCategories } from '@/lib/actions/categoriesAction';
 import { getConditions } from '@/lib/actions/conditionActions';
 import { getStatuses } from '@/lib/actions/statusActions';
-
-import { getAuthUser } from "@/lib/auth";
+import { auth } from '@clerk/nextjs/server';
 
 export default async function PostPage() {
   const categories = await getCategories();
   const condition = await getConditions();
   const statuses = await getStatuses();
-  const { success } = await getAuthUser();
+  const { userId } = await auth();
 
   return (
     <div>
-      {success ? (
+      {userId ? (
         <CreatePostForm categories={categories} conditions={condition} statuses={statuses}/>
       ) : (
         <Sigin />
--- src/components/shared/Sigin.tsx
+++ src/components/shared/Sigin.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import { SignInButton } from '@clerk/nextjs';
 
