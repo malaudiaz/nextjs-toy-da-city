1f8295cf8ca673195d3c33ccb9868d2429d83fa9
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-08-19 10:49:24 -0400

    Arreglos api

--- src/app/[locale]/api/toys/[id]/route.ts
+++ src/app/[locale]/api/toys/[id]/route.ts
@@ -69,6 +69,9 @@ export async function GET(
         },
         _count: {
           select: {
+            likes: {
+              where: { isActive: true }, // Filtra solo comentarios activos
+            },
             comments: {
               where: { isActive: true }, // Filtra solo comentarios activos
             },
