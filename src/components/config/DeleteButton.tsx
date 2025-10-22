26a13d6d4e50441a6090b180ee9ec6472332a48b
migue10david <miguedeveloper23@gmail.com>
2025-10-22 18:58:39 -0400

    dialog

--- src/components/config/DeleteButton.tsx
+++ src/components/config/DeleteButton.tsx
@@ -1,19 +1,43 @@
 "use client";
 import React from "react";
-import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { deleteToy } from "@/lib/actions/toysAction";
+import { Button } from "../ui/button";
 
 const DeleteButton = ({ id }: { id: string }) => {
   const handleDelete = async (id: string) => {
     await deleteToy(id);
   };
   return (
-    <Button
-      className="bg-red-700 hover:bg-red-800 text-white"
-      onClick={() => handleDelete(id)}
-    >
-      Eliminar
-    </Button>
+    <Dialog>
+      <DialogTrigger className="bg-red-700 text-white px-4 rounded-lg">Delete</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your toy
+            and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button variant="destructive">Delete</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
