64eec9ce243070428decea80de25a654d2f70e77
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-12-02 10:01:20 -0500

    arreglos para compilacion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`min-h-screen flex flex-col antialiased font-inter`}>
+        {children}
+      </body>
     </html>
   );
 }
